f458b09d188731f3788bde1ce2a924d2d822dcd8
thangvo38 <thangvo8797@gmail.com>
2018-08-27 17:05:09 +0700

    Var test 7

--- abc.sh
+++ abc.sh
@@ -1,4 +1,10 @@
-x=$(curl \
+version=$(curl \
   -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
   https://rink.hockeyapp.net/api/2/apps/$APP_ID/app_versions | jq -r '.app_versions[0].id')
-echo $x
+echo $version
+
+resignedUrl=$(curl \
+-H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/$APP_ID/app_versions/$version?format=$FILE_FORMAT | jq -r '.headers.location')
+
+echo $resignedUrl
