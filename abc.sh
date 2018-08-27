3158d194606d74fc529f132a7f7d52f3f9b4563d
thangvo38 <thangvo8797@gmail.com>
2018-08-27 16:39:15 +0700

    Var test 6

--- abc.sh
+++ abc.sh
@@ -1,2 +1,4 @@
-x=$(node test.js | jq -r '.key1.key11')
+x=$(curl \
+  -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
+  https://rink.hockeyapp.net/api/2/apps/$APP_ID/app_versions | jq -r '.app_versions[0].id')
 echo $x
